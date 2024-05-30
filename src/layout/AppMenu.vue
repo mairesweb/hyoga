3d6d5aa5c628b39be12dd4d9164a02f5b228663a
Marcelo Aires Vieira <mvieira@nelogica.com.br>
2024-05-30 13:40:31 -0300

    À vista ou parcelado?

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -10,7 +10,7 @@ const model = ref([
             { label: 'Investimentos', icon: 'pi pi-fw pi-chart-line', to: '/' },
             { label: 'Aporte Mensal', icon: 'pi pi-fw pi-calculator', to: '/simulador-aporte-mensal' },
             { label: 'Usufruto de Patrimônio', icon: 'pi pi-fw pi-replay', to: '/simulador-usufruto' },
-            { label: 'Á Vista ou Parcelado', icon: 'pi pi-fw pi-replay', to: '/simulador-compra-parcelada' }
+            { label: 'Á Vista ou Parcelado', icon: 'pi pi-fw pi-shopping-cart', to: '/simulador-compra-parcelada' }
             // { label: 'Visão Geral', icon: 'pi pi-fw pi-chart-bar', to: '/' },
             // { label: 'Carteira', icon: 'pi pi-fw pi-wallet', to: '/carteira' },
             // { label: 'Configurações', icon: 'pi pi-fw pi-chart-pie', to: '/configuracoes' },
--- src/views/simulators/PartialPaymentSimulator.vue
+++ src/views/simulators/PartialPaymentSimulator.vue
@@ -14,19 +14,23 @@ export default {
                 value: null as number | null,
                 option: { name: 'a.m.', code: 'a.m.' }
             },
-            qtyParts: 1,
-            taxe: 0,
             optionsInCash: [
                 { name: 'Valor final', code: 'valor' },
                 { name: 'Dessconto (%)', code: 'percent' }
             ],
             inCash: {
                 value: null as number | null,
-                option: { name: 'Valor final', code: 'valor' }
+                option: { name: 'Valor final', code: 'valor' },
+                discount: null as number | null
             },
             resultSimulation: {
                 inCash: null as number | null,
                 inTerm: null as number | null
+            },
+            inTerm: {
+                qtyParts: 1,
+                taxe: 0,
+                option: { name: 'a.m.', code: 'a.m.' }
             }
         };
     },
@@ -34,55 +38,65 @@ export default {
         clearFields() {
             this.cost = null;
             this.profitability.value = null;
-            this.qtyParts = 1;
-            this.taxe = 0;
-            this.inCash.value = null;
-            this.resultSimulation = null;
+            this.inTerm = {
+                qtyParts: 1,
+                taxe: 0,
+                option: { name: 'a.m.', code: 'a.m.' }
+            };
+            this.inCash = {
+                value: null,
+                option: { name: 'Valor final', code: 'valor' },
+                discount: null
+            };
+            this.resultSimulation = {
+                inCash: null,
+                inTerm: null
+            };
         },
         calculate() {
             if (!this.cost) {
-                this.$toast.add({ severity: 'warn', summary: 'Atenção', detail: 'Informe o valor da compra' });
+                this.$toast.add({ severity: 'warn', summary: 'Atenção', detail: 'Informe o valor da compra', life: 5000 });
+                return;
+            }
+
+            if (!this.inCash.value && !this.inCash.discount) {
+                this.$toast.add({ severity: 'warn', summary: 'Atenção', detail: 'Informe o valor ou o desconto à vista', life: 5000 });
                 return;
             }
 
             if (!this.profitability.value) {
-                this.$toast.add({ severity: 'warn', summary: 'Atenção', detail: 'Informe a rentabilidade' });
-                return;
-            }
-
-            if (!this.inCash.value) {
-                this.$toast.add({ severity: 'warn', summary: 'Atenção', detail: 'Informe o valor à vista' });
+                this.$toast.add({ severity: 'warn', summary: 'Atenção', detail: 'Informe a rentabilidade', life: 5000 });
                 return;
             }
 
             if (this.inCash.option.code === 'percent') {
-                this.inCash.value = this.cost - this.cost * (this.inCash.value / 100);
+                this.inCash.value = this.cost - this.cost * ((this.inCash.discount as number) / 100);
             }
 
-            const totalInCash = this.inCash.option.code === 'valor' ? this.inCash.value : this.calculateDiscount();
+            const totalInCash = this.inCash.option.code === 'valor' ? (this.inCash.value as number) : this.calculateDiscount();
             const discount = this.cost - totalInCash;
-            const totalParts = this.cost / this.qtyParts;
-            const totalPartsWithTaxe = totalParts + totalParts * (this.taxe / 100);
+            const totalParts = this.cost / this.inTerm.qtyParts;
 
             this.resultSimulation.inCash = this.calculateFutureValue(discount);
-            this.resultSimulation.inTerm = this.calcularValorFuturoComRetiradas(totalPartsWithTaxe);
+            this.resultSimulation.inTerm = this.calcularValorFuturoComRetiradas(totalParts, this.getInterestTaxe());
         },
-        calcularValorFuturoComRetiradas(retiradaMensal: number) {
+        calcularValorFuturoComRetiradas(retiradaMensal: number, interestTaxe = 0) {
             let valorFuturo = this.cost;
-            let taxaMensal = this.getTaxe();
+            let taxaMensal = this.getProfitabilityTaxe();
 
-            for (let i = 0; i < this.qtyParts; i++) {
-                valorFuturo = (valorFuturo - retiradaMensal) * (1 + taxaMensal);
+            for (let i = 0; i < this.inTerm.qtyParts; i++) {
+                valorFuturo = ((valorFuturo as number) - retiradaMensal) * (1 + taxaMensal);
+                retiradaMensal *= 1 + interestTaxe;
             }
 
             return valorFuturo;
         },
         calculateDiscount(): number {
-            return this.cost - this.cost * (this.inCash.value / 100);
+            return (this.cost as number) - (this.cost as number) * ((this.inCash.discount as number) / 100);
         },
         calculateFutureValue(investValue: number): number {
-            const taxe = this.getTaxe();
-            const time = this.qtyParts;
+            const taxe = this.getProfitabilityTaxe();
+            const time = this.inTerm.qtyParts;
             const p = investValue;
             const pmt = 0;
             const i = taxe;
@@ -90,8 +104,11 @@ export default {
 
             return Number((p * Math.pow(1 + i, n) + pmt * ((Math.pow(1 + i, n) - 1) / i)).toFixed(2));
         },
-        getTaxe(): number {
+        getProfitabilityTaxe(): number {
             return this.profitability.option.name === 'a.m.' ? (this.profitability.value as number) / 100 : this.convertTaxeToMonthly(this.profitability.value as number);
+        },
+        getInterestTaxe(): number {
+            return this.inTerm.option.name === 'a.m.' ? (this.inTerm.taxe as number) / 100 : this.convertTaxeToMonthly(this.inTerm.taxe as number);
         },
         convertTaxeToMonthly(taxe: number) {
             return Math.pow(1 + taxe / 100, 1 / 12) - 1;
@@ -122,14 +139,18 @@ export default {
                 <label>Pagamento à vista <i class="pi pi-question-circle" v-tooltip.focus.top="'Digite o valor já com desconto ou apenas o percentual descontado.'" tabindex="1"></i></label>
                 <InputGroup>
                     <Dropdown v-model="inCash.option" :options="optionsInCash" optionLabel="name" class="w-12rem" />
-                    <InputNumber v-model="inCash.value" locale="pt-BR" :maxFractionDigits="2" />
+                    <InputGroupAddon v-if="inCash.option.code === 'valor'">R$</InputGroupAddon>
+                    <InputNumber v-if="inCash.option.code === 'valor'" v-model="inCash.value" locale="pt-BR" :maxFractionDigits="2" />
+                    <InputNumber v-else v-model="inCash.discount" locale="pt-BR" :maxFractionDigits="2" />
+                    <InputGroupAddon v-if="inCash.option.code === 'percent'">%</InputGroupAddon>
                 </InputGroup>
             </div>
             <div className="field col-12 md:col-4">
                 <label>Pagamento à prazo <i class="pi pi-question-circle" v-tooltip.focus.top="'Número de parcelas e taxa de juros aplicado na compra parcelada.'" tabindex="2"></i></label>
                 <InputGroup>
-                    <InputNumber v-model="qtyParts" locale="pt-BR" suffix="x" placeholder="Nº de parcelas" :maxFractionDigits="2" />
-                    <InputNumber v-model="taxe" suffix="%" locale="pt-BR" placeholder="Taxa de juros" :maxFractionDigits="2" />
+                    <InputNumber v-model="inTerm.qtyParts" locale="pt-BR" suffix="x" placeholder="Nº de parcelas" :maxFractionDigits="2" />
+                    <InputNumber v-model="inTerm.taxe" suffix="%" locale="pt-BR" placeholder="Taxa de juros" :maxFractionDigits="2" />
+                    <Dropdown v-model="inTerm.option" :options="optionsProfitability" optionLabel="name" class="w-7rem" />
                 </InputGroup>
             </div>
             <div className="field col-12 md:col-4">
