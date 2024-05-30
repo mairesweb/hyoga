f5077c88641127c1a1beb7e5e7f348749b11fe0e
Marcelo Aires Vieira <mvieira@nelogica.com.br>
2024-05-30 15:16:53 -0300

    Ajuste de menu e rotas.

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -8,15 +8,19 @@ const model = ref([
         label: 'Simuladores',
         items: [
             { label: 'Investimentos', icon: 'pi pi-fw pi-chart-line', to: '/' },
-            { label: 'Aporte Mensal', icon: 'pi pi-fw pi-calculator', to: '/simulador-aporte-mensal' },
-            { label: 'Usufruto de Patrimônio', icon: 'pi pi-fw pi-replay', to: '/simulador-usufruto' },
-            { label: 'Á Vista ou Parcelado', icon: 'pi pi-fw pi-shopping-cart', to: '/simulador-compra-parcelada' }
+            { label: 'Aporte Mensal', icon: 'pi pi-fw pi-calculator', to: '/simuladores/aporte-mensal' },
+            { label: 'Usufruto de Patrimônio', icon: 'pi pi-fw pi-replay', to: '/simuladores/usufruto' },
+            { label: 'Á Vista ou Parcelado', icon: 'pi pi-fw pi-shopping-cart', to: '/simuladores/compra-parcelada' }
             // { label: 'Visão Geral', icon: 'pi pi-fw pi-chart-bar', to: '/' },
             // { label: 'Carteira', icon: 'pi pi-fw pi-wallet', to: '/carteira' },
             // { label: 'Configurações', icon: 'pi pi-fw pi-chart-pie', to: '/configuracoes' },
             // { label: 'Aporte', icon: 'pi pi-fw pi-shopping-cart', to: '/aporte' },
             // { label: 'Proventos', icon: 'pi pi-fw pi-dollar', to: '/proventos' }
         ]
+    },
+    {
+        label: 'Gráficos',
+        items: [{ label: 'Liberdade Financeira', icon: 'pi pi-fw pi-chart-line', to: '/graficos/liberdade-financeira' }]
     }
 ]);
 </script>
