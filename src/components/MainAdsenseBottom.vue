077f94f028e8b55b0379b8d531cf7d1b08e4e347
Marcelo Aires Vieira <mvieira@nelogica.com.br>
2024-06-01 14:33:29 -0300

    Ajusta largura dos anúncios

--- src/components/MainAdsenseBottom.vue
+++ src/components/MainAdsenseBottom.vue
@@ -11,7 +11,7 @@ export default defineComponent({
 </script>
 
 <template>
-    <div class="p-fluid flex justify-content-center mt-3 mb-6 main-ad-block" :style="{ width: fixedwidth + 'px' }">
+    <div class="p-fluid flex justify-content-center mt-3 mb-6 main-ad-block" :style="{ width: (fixedwidth || 1024) + 'px' }">
         <Adsense data-ad-client="ca-pub-2781432625181032" data-ad-slot="5627803764" data-ad-format="auto" data-full-width-responsive="true"></Adsense>
     </div>
 </template>
