8a7db59147e8bf703fd0d40e3676748f59ebaee4
Marcelo Aires Vieira <mairesweb@gmail.com>
2024-10-20 14:57:26 -0300

    Install cypress for E2E tests

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,5 +1,7 @@
+/// <reference types="cypress" />
+
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    it('passes', () => {
+        cy.visit('https://example.cypress.io');
+    });
+});
